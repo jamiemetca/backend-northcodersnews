ef3c339a573150ab73c6feee99f2b9ac20d5df94
jamiemetca <jamiemetca@gmail.com>
2018-07-11 10:51:07 +0100

    adjusted /api route. Method to serve html

--- routes/api.js
+++ routes/api.js
@@ -10,7 +10,7 @@ const { userRouter } = require("./user");
 //   );
 // });
 
-apiRouter.use('/', express.static('public'))
+// apiRouter.use('/', express.static('public')) 
 
 apiRouter.use("/topics", topicRouter);
 apiRouter.use("/articles", articleRouter);
